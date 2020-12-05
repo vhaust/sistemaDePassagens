bb4f6f5b5842a9088832bdc571d06031d8683374
vhaust <ghustavofernandes@hotmail.com>
2020-12-04 21:59:27 -0300

    Remove classes não utilizadas

--- src/br/com/ap22020/sistema_de_passagens/model/ClasseModelExemplo.java
+++
@@ -1,5 +0,0 @@
-package br.com.ap22020.sistema_de_passagens.model;
-
-public class ClasseModelExemplo {
-
-}
--- src/br/com/ap22020/sistema_de_passagens/view/ClasseViewExemplo.java
+++
@@ -1,5 +0,0 @@
-package br.com.ap22020.sistema_de_passagens.view;
-
-public class ClasseViewExemplo {
-
-}
